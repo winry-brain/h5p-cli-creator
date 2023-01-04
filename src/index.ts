cfe13b6c4b4f9d342f4d0d3fe9736daaf4728a0d
Ekaterina Durova <ekaterina.durova@gmail.com>
2023-01-04 15:36:28 +0100

    Added memory game. Changed wav audio with mp3

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@
 import * as yargs from "yargs";
 import { DialogCardsModule } from "./dialogcards-module";
 import { FlashcardsModule } from "./flashcards-module";
+import { MemoryGameModule } from "./memorygame-module";
 
 try {
   yargs
     .command(new FlashcardsModule())
     .command(new DialogCardsModule())
+    .command(new MemoryGameModule())
     .help().argv;
 } catch (error) {
   console.error(error);
